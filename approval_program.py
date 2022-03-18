73a67b50429afc1c9b47d9a07ad81ea98429c311
BULO-Coin <101530431+BULO-Coin@users.noreply.github.com>
2022-03-18 23:19:19 +0100

    Added Did_we_succeed, solved a few TODO, improved code security

--- approval_program.py
+++ approval_program.py
@@ -1,3 +1,5 @@
+"""BULO Coin Smartcontract"""
+
 def approval_program():
 
     #When we create the smart contract, we initialise the variables
@@ -27,28 +29,24 @@ def approval_program():
         ).Then(
             Seq(
                 #The contract opts-in BULO by sending a 0 value transacttion to itself
-                #The Bulo id is a constant, fixed outside of the contract logic
-                #TODO: change that
-                App.globalPut(Bytes("BULO_ID"), Int(int(BULOid))),
+                App.globalPut(Bytes("BULO_ID"), Txn.assets[0]),
                 InnerTxnBuilder.Begin(),
                 InnerTxnBuilder.SetFields(
                     {
                         TxnField.type_enum: TxnType.AssetTransfer,
-                        TxnField.xfer_asset: Int(int(BULOid)),
+                        TxnField.xfer_asset: App.globalGet(Bytes("BULO_ID")),
                         TxnField.asset_amount: Int(0),
                         TxnField.asset_receiver: Global.current_application_address(),
                     }
                 ),
                 InnerTxnBuilder.Submit(),
                 #The contract opts-in the Buloed ASA by sending a 0 value transacttion to itself
-                #The RUGPULLid id is a constant, fixed outside of the contract logic
-                #TODO: change that
-                App.globalPut(Bytes("Buloed_ASA_ID"), Int(int(RUGPULLid))),
+                App.globalPut(Bytes("Buloed_ASA_ID"), Txn.assets[1]),
                 InnerTxnBuilder.Begin(),
                 InnerTxnBuilder.SetFields(
                     {
                         TxnField.type_enum: TxnType.AssetTransfer,
-                        TxnField.xfer_asset: Int(int(RUGPULLid)),
+                        TxnField.xfer_asset: App.globalGet(Bytes("Buloed_ASA_ID")),
                         TxnField.asset_amount: Int(0),
                         TxnField.asset_receiver: Global.current_application_address(),
                     }
@@ -83,6 +81,7 @@ def approval_program():
     #Here we want to receive 2 transactions in the group:
     #- the first one is the BULO ASA transfert to be commited
     #- the second one is the call to this function
+    LockUntil_computed = ScratchVar(TealType.uint64)
     send_bulo_txn = Gtxn[Txn.group_index() - Int(1)]
     I_Promise_I_will_not_Rugpull = Seq(
         #Some preliminary checks
@@ -113,7 +112,7 @@ def approval_program():
                 InnerTxnBuilder.SetFields(
                     {
                         TxnField.type_enum: TxnType.AssetTransfer,
-                        TxnField.xfer_asset: Int(int(BULOid)),
+                        TxnField.xfer_asset: App.globalGet(Bytes("BULO_ID")),
                         TxnField.asset_amount: Div(Mul(send_bulo_txn.asset_amount(), Int(2)),Int(100)),
                         TxnField.asset_receiver: App.globalGet(Bytes("RescueWallet_Address")),
                     }
@@ -124,7 +123,7 @@ def approval_program():
                 InnerTxnBuilder.SetFields(
                     {
                         TxnField.type_enum: TxnType.AssetTransfer,
-                        TxnField.xfer_asset: Int(int(BULOid)),
+                        TxnField.xfer_asset: App.globalGet(Bytes("BULO_ID")),
                         TxnField.asset_amount: Div(Mul(send_bulo_txn.asset_amount(), Int(2)),Int(100)),
                         TxnField.asset_receiver: App.globalGet(Bytes("AlgoFaucetWallet_Address")),
                     }
@@ -135,7 +134,7 @@ def approval_program():
                 InnerTxnBuilder.SetFields(
                     {
                         TxnField.type_enum: TxnType.AssetTransfer,
-                        TxnField.xfer_asset: Int(int(BULOid)),
+                        TxnField.xfer_asset: App.globalGet(Bytes("BULO_ID")),
                         TxnField.asset_amount: Div(Mul(send_bulo_txn.asset_amount(), Int(1)),Int(100)),
                         TxnField.asset_receiver: App.globalGet(Bytes("StupidityWallet_Address")),
                     }
@@ -143,10 +142,11 @@ def approval_program():
                 InnerTxnBuilder.Submit(),
                 #We setup the lower limit for the liquidity, to know when the coin is rugpulled
                 App.globalPut(Bytes("Buloed_ASA_LP_lower_limit"), Btoi(Txn.application_args[1])),
-                #We setup the upper limit for the liquidity, to use in conjusction with the lock_until date to know if the coin is successfull
-                #TODO: implement the lock_until date check and the Did_we_succeed function
+                #We setup the upper limit for the liquidity, to use in conjusction with the lock_until date 
+                #to know if the coin is successfull in the Did_we_succeed function
                 App.globalPut(Bytes("Buloed_ASA_LP_upper_limit"), Btoi(Txn.application_args[2])),
-                App.globalPut(Bytes("BULO_locked_until"), Int(0)),
+                LockUntil_computed.store(Add(Global.latest_timestamp(),Mul(Btoi(Txn.application_args[3]),Int(86400)))),
+                App.globalPut(Bytes("BULO_locked_until"), LockUntil_computed.load()),
                 #We store the remaining commited BULO (ammount - 5%)
                 App.globalPut(Bytes("Commited_BULO"), send_bulo_txn.asset_amount() - Div(Mul(send_bulo_txn.asset_amount(), Int(5)),Int(100))),
                 #The setup is complete, we wont be able to call this function anymore
@@ -172,7 +172,7 @@ def approval_program():
             # And confirm it has a value
             Assert(Buloed_ASA_Total.hasValue()),
             If(
-                #We check the provided asset is indeed the Buloed ASA to avoid getting tinymanned
+                #We check the provided asset is indeed the Buloed ASA to avoid id getting tinymanned
                 Txn.assets[1] == App.globalGet(Bytes("Buloed_ASA_ID"))
             ).Then(
                 Seq(
@@ -227,7 +227,7 @@ def approval_program():
                 InnerTxnBuilder.SetFields(
                     {
                         TxnField.type_enum: TxnType.AssetTransfer,
-                        TxnField.xfer_asset: Int(int(BULOid)),
+                        TxnField.xfer_asset: App.globalGet(Bytes("BULO_ID")),
                         #The ammount to send is computed in the ComputeBuloAmmount function
                         TxnField.asset_amount: ComputeBuloAmmount(send_ASA_txn.asset_amount()),
                         TxnField.asset_receiver: Txn.sender(),
@@ -242,10 +242,51 @@ def approval_program():
         Reject(),
     ])
 
-    
+    ##Here we want to receive a single transaction in the group: the app call
+    ##This function checks if the LP exeed the upper threshold and if the lockup day have passed
+    ##If it's the case, it sends half of the BULO to the RescueWallet_Address, and locks itself
+    ##TODO: This needs more testing
+    BULO_Balance = AssetHolding.balance(Txn.sender(), Txn.assets[0])
+    BULO_To_Unlock_computed = ScratchVar(TealType.uint64)
     Did_we_succeed = Seq(
-        #Not yet implemented, that's so sad
-        Approve(),
+        If(
+            And(
+                #If the Balance of the LP address is higher than the limit defined when the contract was setup, the coin is successful
+                Balance(App.globalGet(Bytes("Buloed_ASA_LP_Address"))) <= App.globalGet(Bytes("Buloed_ASA_LP_upper_limit")),
+                Global.latest_timestamp() > App.globalGet(Bytes("BULO_locked_until")),
+                App.globalGet(Bytes("Setup_complete")) == Int(2),
+            ),
+        ).Then(
+            Seq(
+                #We compute half the BULO
+                BULO_Balance,
+                Assert(BULO_Balance.hasValue()),
+                BULO_To_Unlock_computed.store(Div(BULO_Balance.value(),Int(2))),
+                # We sumit an inner transaction to send half the BULO to the Rescue Wallet
+                InnerTxnBuilder.Begin(),
+                InnerTxnBuilder.SetFields(
+                    {
+                        TxnField.type_enum: TxnType.AssetTransfer,
+                        TxnField.xfer_asset: App.globalGet(Bytes("BULO_ID")),
+                        #The ammount is half the BULO
+                        TxnField.asset_amount: BULO_To_Unlock_computed.load(),
+                        TxnField.asset_receiver: App.globalGet(Bytes("RescueWallet_Address")),
+                    }
+                ),
+                InnerTxnBuilder.Submit(),
+                #We prevent this function to be called again
+                App.globalPut(Bytes("Setup_complete"), Int(3)),
+                #We confirm that everything went well
+                Approve(),
+            )
+        )#.Else( #For Testing only
+        #    Seq(
+        #        App.globalPut(Bytes("BULO_lovers_count"), Int(69)),
+        #        Approve(),
+        #    )
+        #)
+        ,
+        Reject(),
     )    
 
     #Depending on the application args and the groyped transaction size, we will execute different parts of the code
